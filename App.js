a299590a37f0af192158bf836aea6004c301826b
jekb2019 <jekb2019@gmail.com>
2021-07-30 13:23:24 +1200

    Add fonts and AppLoading for async font loading

--- App.js
+++ App.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
 import Home from './screens/home';
+import AppLoading from 'expo-app-loading';
+
+// Function that gets the fonts from the assets and register under the name in the key
+const getFonts = () => {
+  return Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Regular.ttf'),
+  })
+};
 
 export default function App() {
-  return (
-    <Home/>
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if(fontsLoaded) {
+    return (
+      <Home/>
+    );
+  } else {
+    return (
+      // 아무것도 보여주지 않지만 startAsync에 정의된 함수가 실행이 된 이후 <Home>을 보여준다
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.warn}
+      />
+    )
+  }
 }
